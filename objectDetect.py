70c491bcc0dee197dab4694fe355b5effdbb6d1c
Oscar <oemolina18@gmail.com>
2024-10-21 14:55:16 -0400

    removed some errors (code might not work)

--- objectDetect.py
+++ objectDetect.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import argparse
-import __future__ import print_function
+import __future__
 
 
 def detectAndDisplay(frame):
@@ -9,13 +9,13 @@ def detectAndDisplay(frame):
  #-- Detect faces
  faces = face_cascade.detectMultiScale(frame_gray)
  for (x,y,w,h) in faces:
- center = (x + w//2, y + h//2)
+    center = (x + w//2, y + h//2)
  frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
  faceROI = frame_gray[y:y+h,x:x+w]
  #-- In each face, detect eyes
  eyes = eyes_cascade.detectMultiScale(faceROI)
  for (x2,y2,w2,h2) in eyes:
- eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+    eye_center = (x + x2 + w2//2, y + y2 + h2//2)
  radius = int(round((w2 + h2)*0.25))
  frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
  cv.imshow('Capture - Face detection', frame)
@@ -44,8 +44,8 @@ if not cap.isOpened:
 while True:
  ret, frame = cap.read()
  if frame is None:
- print('--(!) No captured frame -- Break!')
+    print('--(!) No captured frame -- Break!')
  break
  detectAndDisplay(frame)
  if cv.waitKey(10) == 27:
- break
+    break
